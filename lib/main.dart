b0b047800c85b11fe228aad5c14b9f0bc2ba94bc
Damodar Lohani <dlohani48@gmail.com>
2020-08-18 20:08:46 +0545

    basic page setup

--- lib/main.dart
+++ lib/main.dart
@@ -22,12 +22,130 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter Plugins Tutorials'),
-      ),
-      body: ListView(
-        padding: const EdgeInsets.all(16.0),
-        children: <Widget>[],
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: Stack(
+                children: <Widget>[
+                  Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Colors.pink,
+                      image: DecorationImage(
+                          image: NetworkImage(
+                              "https://images.pexels.com/photos/68525/soap-colorful-color-fruit-68525.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"),
+                          fit: BoxFit.cover),
+                    ),
+                    foregroundDecoration:
+                        BoxDecoration(color: Colors.pink.withAlpha(180)),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(16.0),
+                    width: double.infinity,
+                    child: Column(
+                      children: <Widget>[
+                        Spacer(
+                          flex: 2,
+                        ),
+                        Text(
+                          "existing members",
+                          style: TextStyle(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 16.0,
+                              color: Colors.white),
+                        ),
+                        SizedBox(height: 10.0),
+                        SizedBox(
+                          width: double.infinity,
+                          child: RaisedButton(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(5.0)),
+                            padding: const EdgeInsets.all(16.0),
+                            color: Colors.white,
+                            child: Text(
+                              "Login",
+                              style: TextStyle(color: Colors.pink),
+                            ),
+                            onPressed: () {},
+                          ),
+                        ),
+                        Spacer()
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Container(
+                padding: const EdgeInsets.all(16.0),
+                width: double.infinity,
+                decoration: BoxDecoration(color: Colors.white),
+                child: Column(
+                  children: <Widget>[
+                    Spacer(),
+                    Text("or if you are new here"),
+                    SizedBox(height: 10.0),
+                    SizedBox(
+                      width: double.infinity,
+                      child: RaisedButton(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5.0)),
+                        padding: const EdgeInsets.all(16.0),
+                        color: Colors.pink,
+                        child: Text(
+                          "Sign up",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        onPressed: () {},
+                      ),
+                    ),
+                    SizedBox(height: 20.0),
+                    Text("or continue with"),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        RaisedButton.icon(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(2.0)),
+                          color: Colors.red,
+                          icon: Icon(
+                            Icons.lock,
+                            color: Colors.white,
+                          ),
+                          label: Text(
+                            "Google",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          onPressed: () {},
+                        ),
+                        SizedBox(width: 10.0),
+                        RaisedButton.icon(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(2.0)),
+                          color: Colors.indigo,
+                          icon: Icon(
+                            Icons.lock,
+                            color: Colors.white,
+                          ),
+                          label: Text(
+                            "Facebook",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          onPressed: () {},
+                        ),
+                      ],
+                    ),
+                    Spacer(
+                      flex: 2,
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
