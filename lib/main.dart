917b954b6bbbde7408f49593abbd075cfd8e63ae
Damodar Lohani <dlohani48@gmail.com>
2020-08-21 08:51:59 +0545

    device info final

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:device_info/device_info.dart';
 
 void main() {
   runApp(MyApp());
@@ -19,6 +20,7 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
+  DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,7 +29,32 @@ class HomePage extends StatelessWidget {
       ),
       body: ListView(
         padding: const EdgeInsets.all(16.0),
-        children: <Widget>[],
+        children: <Widget>[
+          FutureBuilder<AndroidDeviceInfo>(
+            future: deviceInfo.androidInfo,
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              if (snapshot.hasData) {
+                AndroidDeviceInfo info = snapshot.data;
+                  return Container(
+                    child: Column(
+                      children: [
+                        Text("${info.brand}",style: Theme.of(context).textTheme.headline5,),
+                        Text("${info.device}",style: Theme.of(context).textTheme.headline5),
+                        Text("${info.model}",style: Theme.of(context).textTheme.headline5),
+                        Text("${info.manufacturer}",style: Theme.of(context).textTheme.headline5),
+                        Text("${info.id}",style: Theme.of(context).textTheme.headline5),
+                        Text("${info.isPhysicalDevice}",style: Theme.of(context).textTheme.headline5),
+                        Text("${info.host}",style: Theme.of(context).textTheme.headline5),
+                        
+                        Text("${info.version.sdkInt}",style: Theme.of(context).textTheme.headline5),
+                      ],
+                    ),
+                  );
+              }
+              return CircularProgressIndicator();
+            },
+          ),
+        ],
       ),
     );
   }
