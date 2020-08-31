de89229addbd7d71cd93809ee4a36311705fadad
Damodar Lohani <dlohani48@gmail.com>
2020-08-31 07:43:07 +0545

    floating action row package

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:floating_action_row/floating_action_row.dart';
 
 void main() {
   runApp(MyApp());
@@ -28,6 +29,31 @@ class HomePage extends StatelessWidget {
       body: ListView(
         padding: const EdgeInsets.all(16.0),
         children: <Widget>[],
+      ),
+      floatingActionButton: FloatingActionRow(
+        color: Colors.redAccent,
+        axis: Axis.vertical,
+        children: [
+          FloatingActionRowButton(
+            icon: Icon(Icons.home),
+            onTap: () {},
+          ),
+          FloatingActionRowDivider(),
+          FloatingActionRowButton(
+            icon: Icon(Icons.message),
+            onTap: () {},
+          ),
+          FloatingActionRowDivider(),
+          FloatingActionRowButton(
+            icon: Icon(Icons.help),
+            onTap: () {},
+          ),
+          FloatingActionRowDivider(),
+          FloatingActionRowButton(
+            icon: Icon(Icons.more_horiz),
+            onTap: () {},
+          ),
+        ],
       ),
     );
   }
