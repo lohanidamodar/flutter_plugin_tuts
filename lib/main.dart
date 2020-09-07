9769a72d1c9c2ceb0b6da4ba2964d253ea578040
Damodar Lohani <dlohani48@gmail.com>
2020-09-07 05:48:49 +0545

    url launcher plugin demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(MyApp());
@@ -27,7 +28,51 @@ class HomePage extends StatelessWidget {
       ),
       body: ListView(
         padding: const EdgeInsets.all(16.0),
-        children: <Widget>[],
+        children: <Widget>[
+          FlatButton(
+            child: Text("Launcher flutter dev"),
+            onPressed: () {
+              launch("https://flutter.dev");
+            },
+          ),
+          FlatButton(
+            child: Text("Contact us"),
+            onPressed: () {
+              launch("tel:+9779818225566");
+            },
+          ),
+          FlatButton(
+            child: Text("Send text"),
+            onPressed: () {
+              launch("sms:+9779818225566");
+            },
+          ),
+          FlatButton(
+            child: Text("Unsupported text"),
+            onPressed: () async {
+              bool canl = await canLaunch("skype://+9779818225566");
+              if (canl) {
+                launch("skype://+9779818225566");
+              } else {
+                print("cannot launch");
+              }
+            },
+          ),
+          FlatButton(
+            child: Text("Send email"),
+            onPressed: () {
+              Uri uri = Uri(
+                scheme: "mailto",
+                path: "popupbits@gmail.com",
+                queryParameters: {
+                  "subject": "Thank you for the tutorial",
+                  "body": "The tutorial regarding url_launcher was awesome"
+                },
+              );
+              launch(uri.toString());
+            },
+          ),
+        ],
       ),
     );
   }
