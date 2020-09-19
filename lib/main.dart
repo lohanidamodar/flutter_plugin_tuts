ebe086dfde241b342c1bd680198a0fed9a2ada99
Damodar Lohani <dlohani48@gmail.com>
2020-09-19 20:55:55 +0545

    simple OCR in flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobile_vision/flutter_mobile_vision.dart';
 
 void main() {
   runApp(MyApp());
@@ -18,16 +19,50 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _ocrCam = FlutterMobileVision.CAMERA_BACK;
+  List<OcrText> _texts = [];
+  @override
+  void initState() {
+    super.initState();
+    FlutterMobileVision.start().then((value) => setState(() {}));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter Plugins Tutorials'),
       ),
-      body: ListView(
-        padding: const EdgeInsets.all(16.0),
-        children: <Widget>[],
+      body: Column(
+        children: [
+          ..._texts.map((t) => Text(
+                t.value,
+                style: TextStyle(fontSize: 18.0),
+              )),
+          RaisedButton(
+            child: Text("Read Text"),
+            onPressed: () async {
+              List<OcrText> texts = [];
+              try {
+                texts = await FlutterMobileVision.read(
+                  camera: _ocrCam,
+                  waitTap: true,
+                );
+                setState(() {
+                  _texts = texts;
+                });
+              } on Exception {
+                texts.add(OcrText('Failed to recognize'));
+              }
+            },
+          ),
+        ],
       ),
     );
   }
